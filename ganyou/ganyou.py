75aecc10c8665948980adf634bec006a476dc267
gagandeepbajwa[C <gagandeepbajwa1998@gmail.com>
2020-08-01 10:06:44 -0700

    Added creating model function and generating image function in ganyou.py

--- ganyou/ganyou.py
+++ ganyou/ganyou.py
@@ -1,6 +1,61 @@
 '''
 File Description: Main file
 '''
+import tensorflow as tf 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt 
+from tensorflow.keras import layers
+
+
+def train():
+    print("Sarting training the model")
+
+def generative_images(model_path=None):
+    print("Starting generating the images")
+
+    #get the genrative model
+    if(model_path==None):
+        #create a new model
+        model=build_model()
+
+        #code from here is just for testign purposes
+        noise = np.random.normal([0,1000])
+        generated_image = model(noise, training=False)
+        print(generated_image.shape)
+
+
+def build_model():
+    print("Starting building the model")
+    
+    #defining the model
+    model = tf.keras.Sequential()
+    model.add(layers.Dense(7*7*256, use_bias=False, input_shape=(100,)))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Reshape((7, 7, 256)))
+    assert model.output_shape == (None, 7, 7, 256) # Note: None is the batch size
+
+    model.add(layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
+    assert model.output_shape == (None, 7, 7, 128)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+    assert model.output_shape == (None, 14, 14, 64)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
+    assert model.output_shape == (None, 28, 28, 1)
+
+    return model
+
+
 
 if __name__=="__main__":
     print("Welcome to GAN")
+    
+    #Calling fucntion to use GAN to genrate images
+    generative_images()
