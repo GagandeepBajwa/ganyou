f4975d9c1f1220bdd2716fdebbec88bee858b14b
gagandeepbajwa[C <gagandeepbajwa1998@gmail.com>
2020-08-02 16:42:56 -0700

    Added build_discrimnative_model fucntion adn impleted generating images with the help of generatigve model

--- ganyou/ganyou.py
+++ ganyou/ganyou.py
@@ -11,21 +11,26 @@ from tensorflow.keras import layers
 def train():
     print("Sarting training the model")
 
-def generative_images(model_path=None):
+def generate_images(model_path=None):
     print("Starting generating the images")
 
     #get the genrative model
     if(model_path==None):
-        #create a new model
-        model=build_model()
+        #create a new generative model
+        generative_model=build_generative_model()
+
+        #create a new discrimator model
+        #discriminative_model = build_discriminative_model()
 
         #code from here is just for testign purposes
-        noise = np.random.normal([0,1000])
-        generated_image = model(noise, training=False)
+        noise = tf.random.normal([1,100])
+        generated_image = generative_model(noise, training=False)
         print(generated_image.shape)
+        plt.imshow(generated_image[0,:,:,0], cmap="cubehelix")
+        plt.show()
 
 
-def build_model():
+def build_generative_model():
     print("Starting building the model")
     
     #defining the model
@@ -52,10 +57,28 @@ def build_model():
 
     return model
 
+def build_discriminative_model():
+    print("Starting building the discriminative model")
 
+    #defining the model
+    model = tf.keras.Sequential()
+
+    model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same',
+                                     input_shape=[28, 28, 1]))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(1))
+
+    return model
 
 if __name__=="__main__":
     print("Welcome to GAN")
     
     #Calling fucntion to use GAN to genrate images
-    generative_images()
+    generate_images()
